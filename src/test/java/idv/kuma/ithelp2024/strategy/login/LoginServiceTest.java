54134b95717fd0177059b51aca1e4d9b503195fb
Yu-Song Syu <kuma@cloud-latitude.com>
2024-07-16 18:00:51 +0800

    dummy test

---
+++ src/test/java/idv/kuma/ithelp2024/strategy/login/LoginServiceTest.java
@@ -0,0 +1,12 @@
+package idv.kuma.ithelp2024.strategy.login;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LoginServiceTest {
+
+    @Test
+    void name() {
+        Assertions.assertThat(1).isEqualTo(1);
+    }
+}
