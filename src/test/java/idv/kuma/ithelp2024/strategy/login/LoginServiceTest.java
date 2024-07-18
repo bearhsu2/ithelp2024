c3082bbc867ed5878495557518d3a364619c8e22
Yu-Song Syu <kuma@cloud-latitude.com>
2024-07-18 22:37:07 +0800

    extract methods

--- src/test/java/idv/kuma/ithelp2024/strategy/login/LoginServiceTest.java
+++ src/test/java/idv/kuma/ithelp2024/strategy/login/LoginServiceTest.java
@@ -3,9 +3,9 @@ package idv.kuma.ithelp2024.strategy.login;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.mockito.stubbing.OngoingStubbing;
 
 import static idv.kuma.ithelp2024.strategy.login.LoginType.GOOGLE;
-import static org.mockito.ArgumentMatchers.anyString;
 
 class LoginServiceTest {
 
@@ -17,24 +17,36 @@ class LoginServiceTest {
     @Test
     void google_login_ok() {
 
-        Mockito.when(googleLoginClient.check(anyString())).thenReturn("kukumama@gmail.com");
+        given_google_token_exists("google_login_token", "kukumama@gmail.com");
 
-        userRepository.add(new User(1L, "kukumama@gmail.com"));
+        given_user(1L, "kukumama@gmail.com");
 
-        loginResultCode = loginService.login(GOOGLE, 1L, "google_login_token");
+        when_login(GOOGLE, 1L, "google_login_token");
 
         Assertions.assertThat(loginResultCode).isEqualTo(LoginResultCode.OK);
 
     }
 
+    private OngoingStubbing<String> given_google_token_exists(String token, String email) {
+        return Mockito.when(googleLoginClient.check(token)).thenReturn(email);
+    }
+
+    private void given_user(long userId, String email) {
+        userRepository.add(new User(userId, email));
+    }
+
+    private void when_login(LoginType loginType, long userId, String token) {
+        loginResultCode = loginService.login(loginType, userId, token);
+    }
+
     @Test
     void google_login_failed() {
 
-        Mockito.when(googleLoginClient.check(anyString())).thenReturn("micky_mouse@gmail.com");
+        given_google_token_exists("google_login_token", "micky_mouse@gmail.com");
 
-        userRepository.add(new User(1L, "kukumama@gmail.com"));
+        given_user(1L, "kukumama@gmail.com");
 
-        loginResultCode = loginService.login(GOOGLE, 1L, "google_login_token");
+        when_login(GOOGLE, 1L, "google_login_token");
 
         Assertions.assertThat(loginResultCode).isEqualTo(LoginResultCode.FAILED);
 
