a7b8ee90888433e5411991a22da0e0c404e000b6
Yu-Song Syu <kuma@cloud-latitude.com>
2024-07-24 18:09:38 +0800

    todo

--- src/main/java/idv/kuma/ithelp2024/strategy/login/LoginService.java
+++ src/main/java/idv/kuma/ithelp2024/strategy/login/LoginService.java
@@ -2,6 +2,9 @@ package idv.kuma.ithelp2024.strategy.login;
 
 public class LoginService {
 
+    // Google login -> GoogleLoginClient (假的)
+    // Facebook login -> FacebookLoginClient (假的)
+
     public LoginResultCode login(long userId, String token) {
         return LoginResultCode.OK;
     }
