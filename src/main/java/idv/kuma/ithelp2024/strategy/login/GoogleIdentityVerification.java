6501cf320771a889ffa64d0b96ada51422c00816
Yu-Song Syu <kuma@cloud-latitude.com>
2024-07-24 23:51:34 +0800

    delegate google verification

---
+++ src/main/java/idv/kuma/ithelp2024/strategy/login/GoogleIdentityVerification.java
@@ -0,0 +1,19 @@
+package idv.kuma.ithelp2024.strategy.login;
+
+public class GoogleIdentityVerification {
+    final GoogleLoginClient googleLoginClient;
+
+    public GoogleIdentityVerification(GoogleLoginClient googleLoginClient) {
+        this.googleLoginClient = googleLoginClient;
+    }
+
+    LoginResultCode verifyGoogle(String token, User user) {
+        String email = googleLoginClient.check(token);
+
+        if (email.equals(user.getEmail())) {
+            return LoginResultCode.OK;
+        } else {
+            return LoginResultCode.FAIL;
+        }
+    }
+}
--- src/main/java/idv/kuma/ithelp2024/strategy/login/LoginService.java
+++ src/main/java/idv/kuma/ithelp2024/strategy/login/LoginService.java
@@ -6,13 +6,13 @@ import org.slf4j.LoggerFactory;
 public class LoginService {
     private static final Logger log = LoggerFactory.getLogger(LoginService.class);
     private final UserRepository userRepository;
-    private final GoogleLoginClient googleLoginClient;
     private final FacebookLoginClient facebookLoginClient;
+    private final GoogleIdentityVerification googleIdentityVerification;
 
     public LoginService(UserRepository userRepository, GoogleLoginClient googleLoginClient, FacebookLoginClient facebookLoginClient) {
 
         this.userRepository = userRepository;
-        this.googleLoginClient = googleLoginClient;
+        googleIdentityVerification = new GoogleIdentityVerification(googleLoginClient);
         this.facebookLoginClient = facebookLoginClient;
     }
 
@@ -24,7 +24,7 @@ public class LoginService {
 
         if (loginType.GOOGLE.equals(loginType)) {
 
-            return verifyGoogle(token, user);
+            return googleIdentityVerification.verifyGoogle(token, user);
         }
 
         if (loginType.FACEBOOK.equals(loginType)) {
@@ -48,12 +48,7 @@ public class LoginService {
     }
 
     private LoginResultCode verifyGoogle(String token, User user) {
-        String email = googleLoginClient.check(token);
 
-        if (email.equals(user.getEmail())) {
-            return LoginResultCode.OK;
-        } else {
-            return LoginResultCode.FAIL;
-        }
+        return googleIdentityVerification.verifyGoogle(token, user);
     }
 }
