05eebfbe6c985aa93dd8fbf5b1c67b25e89ab92b
Yu-Song Syu <kuma@cloud-latitude.com>
2024-07-28 23:59:25 +0800

    fake test

--- src/test/java/idv/kuma/ithelp2024/observer/jackpot/AccumulateJackpotPoolServiceTest.java
+++ src/test/java/idv/kuma/ithelp2024/observer/jackpot/AccumulateJackpotPoolServiceTest.java
@@ -1,7 +1,30 @@
 package idv.kuma.ithelp2024.observer.jackpot;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class AccumulateJackpotPoolServiceTest {
 
+//    @Test
+//    void no_jackpot() {
+//
+//        AccumulateJackpotPoolService sut = new AccumulateJackpotPoolService();
+//
+//        JackpotPool jackpotPool = new JackpotPool();
+//        jackpotPool.setId(3345678L);
+//        jackpotPool.setContributionRateHundredThousandth(35L);
+//        jackpotPool.setAmountHundredThousandth(100_000_00_00L);
+//        jackpotPool.setPrizeCent(300_000_00L);
+//
+//        JackpotPoolRepository jackpotPoolRepository = new JackpotPoolRepository();
+//        jackpotPoolRepository.save(jackpotPool);
+//
+//        sut.accumulate(9527L, 100_00L);
+//
+//        Assertions.assertThat(
+//                jackpotPool.getAmountHundredThousandth()
+//        ).isEqualTo(100_000_35_00L);
+//
+//
+//    }
 }
