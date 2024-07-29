dc90770556695467813c1f20d2326f6d419b2ba9
Yu-Song Syu <kuma@cloud-latitude.com>
2024-07-29 17:36:40 +0800

    todo

--- src/test/java/idv/kuma/ithelp2024/observer/jackpot/AccumulateJackpotPoolServiceTest.java
+++ src/test/java/idv/kuma/ithelp2024/observer/jackpot/AccumulateJackpotPoolServiceTest.java
@@ -5,26 +5,29 @@ import org.junit.jupiter.api.Test;
 
 class AccumulateJackpotPoolServiceTest {
 
-//    @Test
-//    void no_jackpot() {
-//
-//        AccumulateJackpotPoolService sut = new AccumulateJackpotPoolService();
-//
-//        JackpotPool jackpotPool = new JackpotPool();
-//        jackpotPool.setId(3345678L);
-//        jackpotPool.setContributionRateHundredThousandth(35L);
-//        jackpotPool.setAmountHundredThousandth(100_000_00_00L);
-//        jackpotPool.setPrizeCent(300_000_00L);
-//
-//        JackpotPoolRepository jackpotPoolRepository = new JackpotPoolRepository();
-//        jackpotPoolRepository.save(jackpotPool);
-//
-//        sut.accumulate(9527L, 100_00L);
-//
-//        Assertions.assertThat(
-//                jackpotPool.getAmountHundredThousandth()
-//        ).isEqualTo(100_000_35_00L);
-//
-//
-//    }
+    @Test
+    void no_jackpot() {
+
+        AccumulateJackpotPoolService sut = new AccumulateJackpotPoolService();
+
+        JackpotPool jackpotPool = new JackpotPool();
+        jackpotPool.setId(3345678L);
+        jackpotPool.setContributionRateHundredThousandth(35L);
+        jackpotPool.setAmountHundredThousandth(100_000_00_00L);
+        jackpotPool.setPrizeCent(300_000_00L);
+
+        JackpotPoolRepository jackpotPoolRepository = new JackpotPoolRepository();
+        jackpotPoolRepository.save(jackpotPool);
+
+        sut.accumulate(9527L, 100_00L);
+
+        Assertions.assertThat(
+                jackpotPool.getAmountHundredThousandth()
+        ).isEqualTo(100_000_35_00L);
+        // update new pool to big screen
+        // update prize to big screen
+        // update prize to machine
+
+
+    }
 }
