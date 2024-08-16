2ef42c24d043fd0a1518f3b6160f7ecb314df907
Yu-Song Syu <kuma@cloud-latitude.com>
2024-08-17 00:35:03 +0800

    extract fields

--- src/test/java/idv/kuma/ithelp2024/responsibility/chain/reference/AddInvitationServiceTest.java
+++ src/test/java/idv/kuma/ithelp2024/responsibility/chain/reference/AddInvitationServiceTest.java
@@ -11,21 +11,21 @@ import java.util.Optional;
 
 @Disabled
 class AddInvitationServiceTest {
+
+    private UserRepository userRepository = new DummyUserRepository();
+    private UserInvitationRepository userInvitationRepository = new FakeUserInvitationRepository();
+    private AddInvitationService sut = new AddInvitationService(userRepository, userInvitationRepository);
+
     @Test
     void all_ok() {
 
-        UserRepository userRepository = new DummyUserRepository();
         User inviter = new User(1L, "john@gmail.com", "ABCD001");
         User invitee = new User(2L, "mary@gmail.com", "XYZZ996");
 
         userRepository.save(inviter);
         userRepository.save(invitee);
 
-        UserInvitationRepository userInvitationRepository = new FakeUserInvitationRepository();
-        AddInvitationService sut = new AddInvitationService(userRepository, userInvitationRepository);
-
         sut.add(2L, "ABCD001");
-
 
         Optional<UserInvitation> userInvitation = userInvitationRepository.find(invitee.getId());
         Assertions.assertThat(userInvitation).isNotEmpty();
